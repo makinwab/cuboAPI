b3f53c475b48580e95abc555aab5343c0c208a45
andela-bmakinwa <makinwa37@gmail.com>
2016-02-07 17:54:33 +0100

    Make application allow cross-origin requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  before_action :add_allow_credentials_header
+
+  def add_allow_credentials_header
+    response.headers["Access-Control-Allow-Origin"] = request.headers['Origin'] || "*"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+  end
 end
