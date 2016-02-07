4db6fff6d78b7dcec6e51fc29522f35735de760c
andela-bmakinwa <makinwa37@gmail.com>
2016-02-07 12:54:55 +0100

    Create Item model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207114621) do
+ActiveRecord::Schema.define(version: 20160207115344) do
 
   create_table "buckets", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "bucket_id"
+    t.boolean  "done",       default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
